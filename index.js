7c10fac0f3f33d753b00325506c96382d201a25c
Daria Stepanova <d.stepanova1993@gmail.com>
2023-05-08 08:32:48 +0200

    add some style

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const bookmarkSave = document.querySelector();
+const bookmarkSave = document.querySelector(
+  '[data-js="card-list__bookmark-button"]'
+);
+const bookmarkEnabled = document.querySelector(
+  '[data-js="card-list__bookmark-button--enable"]'
+);
+
+bookmarkSave.addEventListener("click", () => {
+  console.log("click");
+  bookmarkEnabled.classList.toggle("card-list__bookmark-svg--clicked");
+});
