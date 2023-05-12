6dc08c408c5f2dec6d5bdeab1257cab08709da6a
Daria Stepanova <d.stepanova1993@gmail.com>
2023-05-12 15:01:11 +0200

    edit some bugs

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const bookmarkSave = document.querySelector(
 const bookmarkEnabled = document.querySelector(
   '[data-js="card-list__bookmark-button--enable"]'
 );
+const cards = document.querySelectorAll('[data-js="question-card"]');
+console.log(cards);
 
 bookmarkSave.addEventListener("click", () => {
-  console.log("click");
-  bookmarkEnabled.classList.toggle("card-list__bookmark-svg--clicked");
+  function saveBookmarks() {
+    bookmarkEnabled.classList.toggle("card-list__bookmark-svg--clicked");
+  }
+});
+
+const cards = document.querySelectorAll('[data-js="question-card"]');
+cards.forEach((card) => {
+  const bookmarkButton = card.querySelector('[data-js="bookmark-button"]');
+  bookmarkButton.addEventListener("click", () => {
+    bookmarkButton.classList.toggle("bookmark-button--enabled");
+    /* cards.classList.toggle("question-card--marked"); */
+  });
+  const answerButton = card.querySelector(
+    '[data-js="question-card__answer-button"'
+  );
 });
