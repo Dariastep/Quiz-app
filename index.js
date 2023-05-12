7eb3aa935177d932a3ed902944353ee442b45fca
Daria Stepanova <d.stepanova1993@gmail.com>
2023-05-12 17:14:54 +0200

    add some js code

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const bookmarkSave = document.querySelector(
-  '[data-js="card-list__bookmark-button"]'
-);
-const bookmarkEnabled = document.querySelector(
-  '[data-js="card-list__bookmark-button--enable"]'
-);
-const cards = document.querySelectorAll('[data-js="question-card"]');
-console.log(cards);
+const cards = document.querySelectorAll('[data-js="card-list__item"]');
 
-bookmarkSave.addEventListener("click", () => {
-  function saveBookmarks() {
-    bookmarkEnabled.classList.toggle("card-list__bookmark-svg--clicked");
-  }
-});
-
-const cards = document.querySelectorAll('[data-js="question-card"]');
 cards.forEach((card) => {
-  const bookmarkButton = card.querySelector('[data-js="bookmark-button"]');
-  bookmarkButton.addEventListener("click", () => {
-    bookmarkButton.classList.toggle("bookmark-button--enabled");
-    /* cards.classList.toggle("question-card--marked"); */
-  });
-  const answerButton = card.querySelector(
-    '[data-js="question-card__answer-button"'
+  const bookmarkSave = card.querySelector(
+    '[data-js="card-list__bookmark-button"]'
   );
+  const bookmarkSvg = card.querySelector(
+    '[data-js="card-list__bookmark-svg--clicked"]'
+  );
+  bookmarkSave.addEventListener("click", () => {
+    bookmarkSvg.classList.toggle("card-list__bookmark-svg--clicked");
+  });
+
+  const answerButton = card.querySelector(
+    '[data-js="card-list__answer-button"]'
+  );
+  const showAnswer = document.querySelector('[data-js="card-list__answer"]');
+  answerButton.addEventListener("click", () => {
+    showAnswer.hidden = false;
+  });
 });
